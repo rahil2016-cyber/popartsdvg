19bb5a0c68d0da10bd82f6db432258b49c881677
faizan003 <faizankhanm63611@gmail.com>
2026-06-02 01:40:25 +0530

    fix: remove auth middleware from createOrder to allow guest checkout

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { createOrder, getMyOrders, getOrderById, getAllOrders, updateOrderStatus } = require('../controllers/orderController');
 const { protect, adminProtect } = require('../middleware/auth');
 
-router.post('/', protect, createOrder);
+router.post('/', createOrder);
 router.get('/my', protect, getMyOrders);
 router.get('/admin', adminProtect, getAllOrders);
 router.get('/:id', getOrderById);
